b658e4655a7abb71ed4f4ea357a16d46f3706534
ThachLN <ThachLN@gmail.com>
2018-11-09 09:30:58 +0700

    Create project Probability-2Boxes: Using Spring boot in Eclipse.

---
+++ source/Probability-2Boxes/src/main/java/mksgroup/ai/learn/probability/Probability2BoxesApplication.java
@@ -0,0 +1,12 @@
+package mksgroup.ai.learn.probability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Probability2BoxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Probability2BoxesApplication.class, args);
+	}
+}
---
+++ source/Probability-2Boxes/src/test/java/mksgroup/ai/learn/probability/Probability2BoxesApplicationTests.java
@@ -0,0 +1,16 @@
+package mksgroup.ai.learn.probability;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Probability2BoxesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
